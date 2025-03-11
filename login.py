eeeccd510db5ad49705462b23c8f3ed58f28ea6a
Abhishek Barde <84563710+abhishekbarde99@users.noreply.github.com>
2025-03-12 00:43:20 +0530

    Update login.py

--- login.py
+++ login.py
@@ -6,12 +6,12 @@ from urllib.parse import quote_plus
 class LoginApp:
     def __init__(self):
 
-        username = "abhibarde1799"
-        password = "iYa4ab7P3FtuTMlh"
+        username = "*******"  # Enter your mongoDb UserName
+        password = "*******"  # Enter Your mongoDb Password
         encoded_username = quote_plus(username)
         encoded_password = quote_plus(password)
 
-        self.client = MongoClient(f"mongodb+srv://{encoded_username}:{encoded_password}@cluster0.kfkd7.mongodb.net/mydatabase?retryWrites=true&w=majority")   # MongoDb Connection String
+        self.client = MongoClient(f"mongodb+srv://{encoded_username}:{encoded_password}@cluster0.kfkd7.mongodb.net/mydatabase?retryWrites=true&w=majority")  
         self.db = self.client["AI_ChatBot"]
         # self.collection = self.db["tbl_userDetails"]
         self.user_collection = self.db["tbl_userDetails"]
@@ -32,7 +32,7 @@ class LoginApp:
         user = self.authenticate_user(username, password)
 
         if user:
-            # ✅ User exists → Fetch chat history
+            #  User exists → Fetch chat history
             st.session_state.user_id = str(user["_id"])
             st.session_state.user_name = user["full_name"]
             st.session_state.is_logged_in = True
@@ -40,7 +40,7 @@ class LoginApp:
             chat_history = self.get_chat_history(st.session_state.user_id)
             return chat_history
         else:
-            # ❌ User does NOT exist → Register them
+            #  User does NOT exist → Register them
             new_user = {"full_name": full_name, "username": username, "password": password}
             inserted_user = self.user_collection.insert_one(new_user)
 
